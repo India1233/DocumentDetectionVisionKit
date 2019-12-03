443c494ffb0fd75bcea2114241eeb433f2b64656
Sachin Prakash Dhumal <sachin.dumal@indigo.co.in>
2019-12-03 17:35:11 +0530

    initial commit

--- DocumentDetectionVisionKit/ViewController.swift
+++ DocumentDetectionVisionKit/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VisionKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func touchUpInsideDocument(_ sender: Any) {
+        setupDocument()
+    }
+    private func setupDocument(){
+        let documentVC = VNDocumentCameraViewController()
+        documentVC.delegate = self
+        self.present(documentVC, animated: true, completion: nil)
+    }
 
 }
 
+
+//MARK:- EXTENSION DocumentVC
+
+extension ViewController: VNDocumentCameraViewControllerDelegate {
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        print("Found \(scan.pageCount)")
+        
+        for i in 0..<scan.pageCount {
+            let img = scan.imageOfPage(at: i)
+            print(img)
+            // ... your code here
+        }
+    }
+}
